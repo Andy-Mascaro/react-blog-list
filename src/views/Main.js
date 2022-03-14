1148a3e2914cc5fb5eaa42b7a9d5ebc058f45998
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-14 12:38:17 -0700

    added blogs file in services folder

---
+++ src/views/Main.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import BlogCard from '../../components/BlogCard/BlogCard';
+import { creatClient } from '../../services/clients'; 
+
+export default function Main() {
+  return (
+    <div>Main</div>
+  )
+}
