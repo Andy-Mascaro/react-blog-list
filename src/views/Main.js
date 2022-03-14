6e66a7648b8c5fe6776a7ab72d3c5e4efa2ea272
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-14 14:24:41 -0700

    added to main js

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import BlogCard from '../../components/BlogCard/BlogCard';
-import { creatClient } from '../../services/clients'; 
+import { fetchBlogs } from '../../services/blogs'; 
+
 
 export default function Main() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => { 
+    const fetch = async () => {
+      try {
+        const resp = await fetchBlogs();
+        setBlogs(resp);
+      } catch (error) { 
+        alert(error.message);
+      }
+    };
+    fetch();
+  }, []);
   return (
-    <div>Main</div>
+    <>
+
+    </>
   )
 }
